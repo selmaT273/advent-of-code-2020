c16cfb54e8edaf37bd61ebde827014e16ccf45d8
Stacey Teltser <stacey.teltser@gmail.com>
2020-12-02 13:32:25 -0600

    converted values from strings to integers and sorted the array

--- day1/day1puzzle1.js
+++ day1/day1puzzle1.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('inputday1puzzle1.txt');
+const data = fs.readFileSync('day1/inputday1puzzle1.txt');
 
 // converts text file data to a string array
 function convertToArray(textData) {
     return textData.toString().split("\n");
 }
 
-// convert to int array
-// sort int array
+// pass in data from txt file
+const stringData = convertToArray(data);
+
+// convert to int array and sort 
+const intData = stringData.map(Number).sort();
+
 // counter on each end, with if condition to check if too high or too low
 
-const numData = convertToArray(data);
+
 
 
 module.exports = convertToArray;
