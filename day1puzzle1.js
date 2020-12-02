e5891ef92bfc07034115664891635e349ba1d6e0
Stacey Teltser <stacey.teltser@gmail.com>
2020-12-02 13:18:38 -0600

    additional steps laid out

--- day1puzzle1.js
+++ day1puzzle1.js
@@ -2,8 +2,16 @@ const fs = require('fs');
 
 const data = fs.readFileSync('inputday1puzzle1.txt');
 
+// converts text file data to a string array
 function convertToArray(textData) {
     return textData.toString().split("\n");
 }
+
+// convert to int array
+// sort int array
+// counter on each end, with if condition to check if too high or too low
+
+const numData = convertToArray(data);
+
 
 module.exports = convertToArray;
--- day1puzzle1.test.js
+++ day1puzzle1.test.js
@@ -5,7 +5,6 @@ const convertToArray = require('./day1puzzle1');
 const fs = require('fs');
 
 test('can read array', () => {
-    
     const data = fs.readFileSync('inputday1puzzle1.txt');
     expect(convertToArray(data)[0]).toBe('1941');
 })
